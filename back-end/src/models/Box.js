95f88e1b4ca64e37e55f6c9005590a94de2c8ffe
Andrey <andreybenny@gmail.com>
2021-04-09 16:48:48 -0300

    Remoção do back-end em mongo/graphql, para execução de back-end em java e mysql

--- back-end/src/models/Box.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const BoxSchema = new Schema({
-    name: String,
-    project: String,
-    sprints: [String]
-});
-
-module.exports = model("Box", BoxSchema);
--- back-end/src/typeDefs/index.js
+++
@@ -1,7 +0,0 @@
-const query = require("./query");
-const mutation = require("./mutations");
-const types = require("./types");
-
-const typeDefs = [query, mutation, types];
-
-module.exports = typeDefs;
