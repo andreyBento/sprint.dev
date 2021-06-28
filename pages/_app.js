9b051e60d6e2cc75b66c08592dab4efaef697b97
Andrey <andreybenny@gmail.com>
2021-06-28 20:21:25 -0300

    Ajuste de favicon e meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,13 @@ function MyApp({ Component, pageProps }) {
   return (
       <div>
         <Head>
-          <meta charSet="utf-8"/>
-          <meta name="theme-color" content="#547DEF" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-          <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"/>
-          <meta name="description" content="Projeto de organização de Sprints."/>
-          <meta name="keywords" content="Sprintdev, sprint, organização de sprints"/>
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <title>Sprint.dev</title>
+            <meta name="theme-color" content="#547DEF" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content="Projeto de organização de Sprints."/>
+            <meta name="keywords" content="Sprintdev, sprint, organização de sprints"/>
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <title>Sprint.dev</title>
         </Head>
           <Provider session={pageProps.session}>
             <Component {...pageProps} />
